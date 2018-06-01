9d226fd77b9f9c14153d1028ef288f2e9d278b54
pedrogallon <pedrogallonalves@hotmail.com>
2018-05-31 21:59:53 -0300

    agora sim finalizado

--- src/Exercicio7_2/ComparatorDoubles.java
+++ src/Exercicio7_2/ComparatorDoubles.java
@@ -1,30 +1,40 @@
 package Exercicio7_2;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 
 public class ComparatorDoubles implements Comparator {
 
+	public ComparatorDoubles() {};
 	public void run(double[] d) {
+		System.out.println("Entendi foi nada desse exercício");
 		int[] i = new int[d.length];
 		for (int x = 0; x<i.length; x++) {
 			i[x] = (int) d[x];
 		}
 		if (compare(d,i) == 1) {
-			
+			System.out.println("São Iguais");
 		}else {
-			
+			System.out.println("Há Diferenças");
+		}
+		
+		System.out.print(i[0]);
+		for (int x = 1; x<i.length; x++) {
+			System.out.print(", "+d[x]);
 		}
 
 	}
 
 	@Override
 	public int compare(Object a, Object b) {
+		int result = 1;
 		double[] d = (double[]) a;
 		int[] i = (int[]) b;
 		for (int x = 0; x<d.length; x++) {
-			if (i)
+			if (i[x] !=  d[x] ) {
+				result = 0;
+			}
 		}
-		return 1;
+		
+		return result;
 	}
 }
--- src/Exercicio7_2/Teste.java
+++ src/Exercicio7_2/Teste.java
@@ -4,7 +4,8 @@ public class Teste {
 
 	public static void main(String[] args) {
 		double[] d = { 1.0, 1.1, 2.0, 2.1 };
-		
+		ComparatorDoubles cd = new ComparatorDoubles();
+		cd.run(d);
 	}
 
 }
